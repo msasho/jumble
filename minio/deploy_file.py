9c0f47d4b9d0981ddd0857ff2203ef3e45e246b3
masawai <secretariat.tmopera@gmail>
2022-09-25 03:05:32 +0000

    変数名修正

--- minio/deploy_file.py
+++ minio/deploy_file.py
@@ -5,13 +5,11 @@ import boto3
 
 BUCKET_NAME = sys.argv[1]
 
-
-source_path = './test_data/'
-target_path = 'test'
-
+SOURCE_PATH = './test_data/'
+TARGET_PATH = 'test'
 
 s3 = boto3.resource(
-    service_name='s3', 
+    service_name='s3',
     endpoint_url='http://localhost:9000',
     aws_access_key_id='localid',
     aws_secret_access_key='localpassword',
@@ -19,11 +17,10 @@ s3 = boto3.resource(
 bucket = s3.Bucket(BUCKET_NAME)
 
 def main():
-    path_dir = './test_data/'
-    list_file_name =  os.listdir(path_dir)
+    list_file_name =  os.listdir(SOURCE_PATH)
     for file in list_file_name:
-        bucket.upload_file(f'{path_dir}/{file}', f'{target_path}/{file}')
+        bucket.upload_file(f'{SOURCE_PATH}/{file}', f'{TARGET_PATH}/{file}')
 
 if __name__ == '__main__':
    main()
-        
+   
